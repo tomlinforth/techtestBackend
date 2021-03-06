70b340570d554fad8c5411292c593feb7ed29879
Tom Linforth <tom.linforth1@gmail.com>
2021-03-06 23:16:53 +0000

    heroku hosting fixes

--- errors/index.js
+++ errors/index.js
@@ -1,16 +1,18 @@
 exports.psqlErr = (err, req, res, next) => {
-    if (err.code) {
-      const errRef = {
-        "22P02": { status: 400, msg: "Invalid data type (integer needed)." },
-        "23503": { status: 404, msg: "Specified ID does not exist." },
-        "23502": { status: 400, msg: "Missing data in request." }
-      };
-      res.status(errRef[err.code].status).send({ msg: errRef[err.code].msg });
-    } else {
-      next(err);
-    }
-  };
+  console.log('uh oh')
+  console.log(err)
+  if (err.code) {
+    const errRef = {
+      "22P02": { status: 400, msg: "Invalid data type (integer needed)." },
+      "23503": { status: 404, msg: "Specified ID does not exist." },
+      "23502": { status: 400, msg: "Missing data in request." }
+    };
+    res.status(errRef[err.code].status).send({ msg: errRef[err.code].msg });
+  } else {
+    next(err);
+  }
+};
   
-  exports.customErr = (err, req, res, next) => {
-    res.status(err.status || 404).send({ err });
-  };
+exports.customErr = (err, req, res, next) => {
+  res.status(err.status || 404).send({ err });
+};
