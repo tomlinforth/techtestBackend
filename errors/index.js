299a8118ee1051ab3004fcec05ae461082b10967
Tom Linforth <tom.linforth1@gmail.com>
2021-03-06 17:23:33 +0000

    error handling default http status fix

--- errors/index.js
+++ errors/index.js
@@ -2,7 +2,6 @@ exports.psqlErr = (err, req, res, next) => {
     if (err.code) {
       const errRef = {
         "22P02": { status: 400, msg: "Invalid data type (integer needed)." },
-        "22003": { status: 400, msg: "Invalid ID (number too long)." },
         "23503": { status: 404, msg: "Specified ID does not exist." },
         "23502": { status: 400, msg: "Missing data in request." }
       };
@@ -13,5 +12,5 @@ exports.psqlErr = (err, req, res, next) => {
   };
   
   exports.customErr = (err, req, res, next) => {
-    res.status(err.status).send({ msg: err.msg });
+    res.status(err.status || 404).send({ msg: err.msg });
   };
