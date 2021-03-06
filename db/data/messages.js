9d2cfe6cd4f02bd64ee90c7e723829fad64501f6
Tom Linforth <tom.linforth1@gmail.com>
2021-03-06 16:50:53 +0000

    fixes for heroku hosting

--- db/data/messages.js
+++ db/data/messages.js
@@ -1,26 +1,8 @@
 module.exports = [
     {
-        contact_number: '01234 567 890',
-        body: "Hi, it's Jane Doe, this is my new phone number.",
-        to: "02345 678 901",
-        from: "01234 567 890"
-    },
-    {
-        contact_number: '02345 678 901',
-        body: "Hi, it's John Smith texting about your ebay listing of a desk.",
-        to: "03456 789 012",
-        from: "02345 678 901"
-    },
-    {
-        contact_number: '03456 789 012',
-        body: "Hey, see the game last night? James.",
-        to: "04567 890 123",
-        from: "03456 789 012"
-    },
-    {
-        contact_number: '04567 890 123',
-        body: "Hello, your payment has failed. Please get in contact with me, Jessica, for further information.",
-        to: "02345 678 901",
-        from: "04567 890 123"
+        contact_number: '+14155238886',
+        body: "Placeholder message.",
+        to: "+447590324815",
+        from: "+14155238886"
     }
 ]
