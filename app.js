4550414b361cf32c5acd612e0c69ae8627ed3289
Tom Linforth <tom.linforth1@gmail.com>
2021-03-06 23:06:28 +0000

    heroku hosting fixes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const apiRouter = require("./routers/api-router");
 const { psqlErr, customErr } = require("./errors/index");
-const app = express();
+const cors = require('cors');
 
+const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use("/api", apiRouter);
