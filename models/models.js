b46c05c5baa13b41231bd1416d7cf5739fbb7cc5
Tom Linforth <tom.linforth1@gmail.com>
2021-03-08 02:06:53 +0000

    added potential twilio messaging

--- models/models.js
+++ models/models.js
@@ -1,4 +1,7 @@
 const connection = require("../db/connection");
+const accountSid = 'AC474d05a343981b3b30086e2ca14c136c';
+const authToken = 'f7af53a21d82107cf9b08c11b4e9b08b';
+const client = require('twilio')(accountSid, authToken);
 
 checkIfContactExists = (contactNum) => {
     return connection('contacts')
@@ -17,10 +20,22 @@ exports.insertNewMessage = (newMessage) => {
         from : newMessage.From, 
         body : newMessage.Body
     };
+    if(newMessage.From == '+12286410309') {
+        return client.messages
+        .create({
+            body: messageInp,
+            from: '+12286410309',
+            to: this.props.curContact
+        }).then(message => {
+            return connection('messages')
+            .insert(messageData)
+            .returning('*');
+        })
+    } else {
     return connection('messages')
         .insert(messageData)
         .returning('*');
-}
+}}
 
 exports.selectAllContacts = () => {
     return connection("contacts").select("*")
